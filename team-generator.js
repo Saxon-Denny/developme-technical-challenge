0e518a5e944574cb66a7ec3e007a51009b3e1830
saxon denny <saxondenny@saxons-MacBook.local>
2019-01-02 21:12:23 +0000

    main MVP functionality JS without styling - status working

--- team-generator.js
+++ team-generator.js
@@ -1,78 +1,83 @@
 (d => {
 
-   
     let random = d.getElementById("random");
     let list = d.getElementById("list");
     let teamOne = d.getElementById("team-one");
     let teamTwo = d.getElementById("team-two");
-    var asideSelector = d.getElementById("myList");
-    var title = document.getElementById("title");
+    let asideSelector = d.getElementById("myList");
+    let title = document.getElementById("title");
+    let teamNumber = asideSelector.options[asideSelector.selectedIndex].value;
     
-    
+    let namesStore = [];
 
- 
-    let store = [];
-    console.log(store);
-
-    
     asideSelector.addEventListener("change", () => {
- 
-        let teamNumber = asideSelector.options[asideSelector.selectedIndex].value;
+        teamNumber = asideSelector.options[asideSelector.selectedIndex].value;
         title.innerHTML= 'Please enter ' + teamNumber + ' names';
     });  
-  
+
+    function validateNames(names) {
+        if(Number(teamNumber) === 0){
+            return 'Please select a valid team number';
+        }
+
+        if(names.length < Number(teamNumber)) {
+            return 'You must enter ' + teamNumber +  ' names';
+        }
+
+        if(names.length > Number(teamNumber)) {
+            return 'You can only enter ' + teamNumber +  ' names';
+        }
+    } 
+    
+    function cleanNames(names){
+        let cleanedNames = [];
+
+         for (i = 0; i < names.length; i++){
+            if (names[i] !== ''){
+                cleanedNames.push(names[i]);
+            }
+        };
+
+        return cleanedNames;
+    }
 
     random.addEventListener("click", () => {
-
-        store = list.value.split('\n');
         
-        //for(var i = 0; i < store.length; i++) {
-           // if (store[i] === '') {
-            //    alert('please enter 10 names')
-            //}else{
+        namesStore = cleanNames(list.value.split('\n'));
 
+        let validationMessage = validateNames(namesStore);
+        if (validationMessage != null){
+            alert(validationMessage);
+            return;
+        }
 
-            let counter = store.length;
-            let teamNumber = asideSelector.options[asideSelector.selectedIndex].value;
-           
-            if(counter === Number(teamNumber)){
-                while (counter > 0) {
-                    let index = Math.floor(Math.random() * counter);
-                    counter--;
-                    let temp = store[counter];
-                    store[counter] = store[index];
-                    store[index] = temp;
-                }
-                
-                let teamOneStore = [];
-                let teamTwoStore = [];
+        let counter = namesStore.length;
+        
+        while (counter > 0) {
+            let index = Math.floor(Math.random() * counter);
+            counter--;
+            let temp = namesStore[counter];
+            namesStore[counter] = namesStore[index];
+            namesStore[index] = temp;
+        }
+            
+        let teamOneStore = [];
+        let teamTwoStore = [];
 
-            for (i = 0; i < store.length; i++){
-                if ( i % 2 === 0){
-                    teamOneStore.push(store[i]);
-                }else{
-                    teamTwoStore.push(store[i]);
-                }
-            }
-                
-                populateListOnDom (list, store);
-                clearList(list);
-                store = [];
-
-                populateListOnDom (teamOne, teamOneStore);
-                console.log(teamOneStore);
-                populateListOnDom (teamTwo, teamTwoStore);
-
-            }else if(Number(teamNumber) === 0){
-                alert('Please select a valid team number');
-
-            }else if(counter < Number(teamNumber)) {
-                alert('You must enter ' + teamNumber +  ' names');
-
+        for (i = 0; i < namesStore.length; i++){
+            if ( i % 2 === 0){
+                teamOneStore.push(namesStore[i]);
             }else{
-                alert('You can only enter ' + teamNumber +  ' names')
+                teamTwoStore.push(namesStore[i]);
             }
-       // }}
+        }
+            
+        populateListOnDom (list, namesStore);
+        clearList(list);
+        namesStore = [];
+
+        populateListOnDom (teamOne, teamOneStore);
+        populateListOnDom (teamTwo, teamTwoStore);  
     });
 
     function populateListOnDom(paramList, paramStore){
@@ -92,6 +97,5 @@
     function clearList(paramList){
         paramList.textContent = "";
     }
-
 
 })(document);
